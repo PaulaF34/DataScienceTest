d2b8496cc890fef7daebada93c9feb8a20825c90
Paula Francis <paulaffrancis@gmail.com>
2024-12-26 01:18:35 +0200

    Insconsistency in data

--- readdata.py
+++ readdata.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+#read the data from the csv file
 df = pd.read_csv('dataset.csv')
 
 #print data
@@ -26,17 +27,22 @@ df = pd.read_csv('dataset.csv')
 # print("Numerical columns: ", numerical_cols)
 # print("Categorical columns: ", categorical_cols)
 
+
+
 #drop the rows with missing values
 # df = df.dropna(axis=0)  # axis=0 means row, axis=1 means column
 
 
-data = {
-    'Name': ['Tom', 'Nick', 'John', 'Nick'],
-    'Age': [20, 21, 19, 21],
-    'City': ['New York', 'Los Angeles', 'Chicago', 'Los Angeles']
-}
 
-df = pd.DataFrame(data)
+#Handle Duplicate Records
+#read the data from data object
+# data = {
+#     'Name': ['Tom', 'Nick', 'John', 'Nick'],
+#     'Age': [20, 21, 19, 21],
+#     'City': ['New York', 'Los Angeles', 'Chicago', 'Los Angeles']
+# }
+
+# df = pd.DataFrame(data)
 # print("Original data frame:")
 # print(df)
 
@@ -68,3 +74,41 @@ df = pd.DataFrame(data)
 # df_keep_last = df.drop_duplicates(keep='last')
 # print("Data frame after dropping duplicates, keep last:")
 # print(df_keep_last)
+
+
+
+
+#Insconsistency in data
+
+data = {
+    'Name': ['Tom', 'Nick', 'John', 'Nick', 'Paula'],
+    'City': ['New York ', 'Los Angeles', 'Chicago', 'LA' , ' los angeles'],
+    'Income': [70000, 80000, 120000, 90000, 100000]
+}
+
+df = pd.DataFrame(data)
+print("Original data frame:")
+print(df)
+
+#Standardize the data in the City column to lowercase
+df['City'] = df['City'].str.lower()
+print("Data frame after standardizing the City column:")
+print(df)
+
+#remove leading and trailing whitespaces
+df['City'] = df['City'].str.strip()
+print("Data frame after removing leading and trailing whitespaces:")
+print(df)
+
+#replace the values in the City column
+city_mapping = {
+    'la': 'los angeles'
+}
+df['City'] = df['City'].replace(city_mapping)
+print("Data frame after replacing values in the City column:")
+print(df)
+
+#Validate unique values in the City column
+unique_categories = df['City'].unique()
+print("Unique values in the City column:")
+print(unique_categories)
